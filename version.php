bb94529a3ebd48479b0aba77fc07c66e4cd72802
lucas040201 <lucasdasilvamendes123456@hotmail.com>
2026-01-22 18:54:40 -0300

    fix: ajusta versão requisitada do moodle

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_contractactivity';
 $plugin->version   = 2026012200;
-$plugin->requires  = 2023081400;
+$plugin->requires  = 2020110300;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0';
